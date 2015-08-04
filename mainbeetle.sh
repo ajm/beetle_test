b11f35a8a7907c5877bbf3c0e1ce244535f6f0b2
Vamiyn <vamiynr@gmail.com>
2015-08-04 15:56:17 +0300

    fixed comments

--- mainbeetle.sh
+++ mainbeetle.sh
@@ -13,10 +13,10 @@ mv Beetles/* Excluded/
 # to make sure sets have matching genes
 find Flies -name '*.fasta' -print0 | xargs -0 -I {} sh -c 'base=$(basename $1) ; name=${base%.*} ; ext=${base##*.} ; echo "$base"' -- {} -print0 | sed -e s/[^0-9]//g | xargs -I % find Excluded -name '*[a-z]%.*' | xargs -I % mv % Beetles
 
-# run SLR on the flies, create trees with RAxML
+# run SLR on the flies, use Ensembl trees
 find Flies -name 'fly*.fasta' -print0 | xargs -P 40 -0 -I {} sh -c 'base=$(basename $1) ; name=${base%.*} ; ext=${base##*.} ; python /home/lauralaa/Scripts_beetle/run_slr.py "$1" n > "Flies/${name}.log"' -- {}
 
-# run SLR on the beetles, using Ensembl trees
+# run SLR on the beetles, create trees with RAxML
 find Beetles -name 'msa*.fasta' -print0 | xargs -P 40 -0 -I {} sh -c 'base=$(basename $1) ; name=${base%.*} ; ext=${base##*.} ; python /home/lauralaa/Scripts_beetle/run_slr.py "$1" y > "Beetles/${name}.log"' -- {}
 
 # again to make sure sets have matching genes
